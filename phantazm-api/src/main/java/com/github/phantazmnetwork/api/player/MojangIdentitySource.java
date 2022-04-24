61ff75a2464745d2ffad2bbea64fd094be3a97f9
John-DND <dnd.initiative.tracker@gmail.com>
2022-04-23 21:28:05 -0700

    switch to minestom fork, add logging to MojangIdentitySource

--- phantazm-api/src/main/java/com/github/phantazmnetwork/api/player/MojangIdentitySource.java
+++ phantazm-api/src/main/java/com/github/phantazmnetwork/api/player/MojangIdentitySource.java
@@ -5,6 +5,8 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import net.minestom.server.utils.mojang.MojangUtils;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 import java.util.Optional;
@@ -13,13 +15,15 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
 /**
- * An {@link IdentitySource} that calls Mojang's API servers to resolve names and UUIDs. Not part of the public API.
+ * An {@link IdentitySource} that calls Mojang's API servers to resolve names and UUIDs. Will log any errors that occur
+ * during resolution using its own dedicated {@link Logger}. Not part of the public API.
  *
  * @apiNote Currently, this utilizes {@link MojangUtils} utility methods and does not perform any caching of its own,
  * aside from that utilized by MojangUtils itself.
  */
 @SuppressWarnings("ClassCanBeRecord")
 class MojangIdentitySource implements IdentitySource {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MojangIdentitySource.class);
     private static final String NAME_KEY = "name";
     private static final String ID_KEY = "id";
 
@@ -32,7 +36,14 @@ class MojangIdentitySource implements IdentitySource {
     @Override
     public @NotNull CompletableFuture<Optional<String>> getName(@NotNull UUID uuid) {
         return CompletableFuture.supplyAsync(() -> {
-            JsonObject response = MojangUtils.fromUuid(uuid.toString());
+            JsonObject response = null;
+            try {
+                response = MojangUtils.fromUuid(uuid.toString());
+            }
+            catch (RuntimeException exception) {
+                LOGGER.error("RuntimeException thrown during username resolution", exception);
+            }
+
             if(response != null) {
                 JsonElement nameElement = response.get(NAME_KEY);
                 if(nameElement != null && nameElement.isJsonPrimitive()) {
@@ -43,6 +54,7 @@ class MojangIdentitySource implements IdentitySource {
                 }
             }
 
+            LOGGER.error("Unable to resolve UUID {}, response was {}", uuid, response);
             return Optional.empty();
         }, executor);
     }
@@ -50,9 +62,16 @@ class MojangIdentitySource implements IdentitySource {
     @Override
     public @NotNull CompletableFuture<Optional<UUID>> getUUID(@NotNull String name) {
         return CompletableFuture.supplyAsync(() -> {
-            JsonObject object = MojangUtils.fromUsername(name);
-            if(object != null) {
-                JsonElement id = object.get(ID_KEY);
+            JsonObject response = null;
+            try {
+                response = MojangUtils.fromUsername(name);
+            }
+            catch (RuntimeException exception) {
+                LOGGER.error("RuntimeException thrown during UUID resolution", exception);
+            }
+
+            if(response != null) {
+                JsonElement id = response.get(ID_KEY);
                 if(id != null && id.isJsonPrimitive()) {
                     JsonPrimitive idPrimitive = id.getAsJsonPrimitive();
                     if(idPrimitive.isString()) {
@@ -61,6 +80,7 @@ class MojangIdentitySource implements IdentitySource {
                 }
             }
 
+            LOGGER.error("Unable to resolve username {}, response was {}", name, response);
             return Optional.empty();
         }, executor);
     }
