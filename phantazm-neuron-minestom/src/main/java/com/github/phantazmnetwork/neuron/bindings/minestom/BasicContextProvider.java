8aa3c45446589728b72e1d85dfa9c0dee74ecb4c
John-DND <dnd.initiative.tracker@gmail.com>
2022-04-26 15:38:48 -0700

    shutdown executor services when instances unregister

--- phantazm-neuron-minestom/src/main/java/com/github/phantazmnetwork/neuron/bindings/minestom/BasicContextProvider.java
+++ phantazm-neuron-minestom/src/main/java/com/github/phantazmnetwork/neuron/bindings/minestom/BasicContextProvider.java
@@ -3,6 +3,7 @@ package com.github.phantazmnetwork.neuron.bindings.minestom;
 import com.github.phantazmnetwork.neuron.engine.*;
 import com.github.phantazmnetwork.neuron.world.Collider;
 import com.github.phantazmnetwork.neuron.world.SpatialCollider;
+import it.unimi.dsi.fastutil.Pair;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.event.instance.InstanceUnregisterEvent;
 import net.minestom.server.instance.Instance;
@@ -10,10 +11,12 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 import java.util.WeakHashMap;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class BasicContextProvider implements ContextProvider {
-    private final Map<Instance, PathContext> contextMap;
+    private final Map<Instance, Pair<ExecutorService, PathContext>> contextMap;
 
     private final int instanceThreads;
     private final int instanceCache;
@@ -22,17 +25,32 @@ public class BasicContextProvider implements ContextProvider {
         this.contextMap = new WeakHashMap<>();
         this.instanceThreads = instanceThreads;
         this.instanceCache = instanceCache;
+
+        MinecraftServer.getGlobalEventHandler().addListener(InstanceUnregisterEvent.class, this::onInstanceUnregister);
     }
 
     @Override
     public @NotNull PathContext provideContext(@NotNull Instance instance) {
         synchronized (contextMap) {
             return contextMap.computeIfAbsent(instance, newInstance -> {
-                PathEngine engine = new BasicPathEngine(Executors.newFixedThreadPool(instanceThreads));
+                ExecutorService service = Executors.newFixedThreadPool(instanceThreads);
+                PathEngine engine = new BasicPathEngine(service);
                 Collider collider = new SpatialCollider(new InstanceSpace(newInstance));
                 PathCache cache = new BasicPathCache(instanceCache);
-                return new BasicPathContext(engine, collider, cache);
-            });
+                return Pair.of(service, new BasicPathContext(engine, collider, cache));
+            }).right();
+        }
+    }
+
+    private void onInstanceUnregister(@NotNull InstanceUnregisterEvent event) {
+        Pair<ExecutorService, PathContext> context;
+        synchronized (contextMap) {
+            context = contextMap.remove(event.getInstance());
+        }
+
+        if(context != null) {
+            ExecutorService executor = context.left();
+            executor.shutdown();
         }
     }
 }
