6e195f6ec4f143663684d9205d1d44467921b4b3
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2022-05-25 09:01:43 -0400

    ELIMINATE the cringe build.gradle and cleanup the version catalog some

---
+++ phantazm-zombies-mapeditor/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("phantazm.java-library-conventions")
+
+    // this IntelliJ error is a false-positive, just ignore it: https://youtrack.jetbrains.com/issue/KTIJ-19369/False-positive-can-t-be-called-in-this-context-by-implicit-recei
+    @Suppress("UnstableApiUsage")
+    alias(libs.plugins.fabric.loom)
+}
+
+base {
+    archivesName.set("phantazm-zombies-mapeditor")
+}
+
+dependencies {
+    minecraft(libs.minecraft)
+    mappings(libs.yarn.mappings) {
+        artifact {
+            classifier = "v2"
+        }
+    }
+    modImplementation(libs.bundles.fabric)
+
+    implementation(project(":phantazm-commons"))
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+}
+
+tasks.compileJava {
+    options.release.set(java.toolchain.languageVersion.get().asInt())
+}
+
+tasks.jar {
+    from("LICENSE") {
+        rename {
+            "${it}_${base.archivesName}"
+        }
+    }
+}
