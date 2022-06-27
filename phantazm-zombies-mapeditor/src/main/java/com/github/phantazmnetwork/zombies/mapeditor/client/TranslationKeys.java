2b71f0a511062e31fe2d82926448765210d2607d
steank <dnd.initiative.tracker@gmail.com>
2022-06-27 08:01:43 -0700

    english localization

--- phantazm-zombies-mapeditor/src/main/java/com/github/phantazmnetwork/zombies/mapeditor/client/TranslationKeys.java
+++ phantazm-zombies-mapeditor/src/main/java/com/github/phantazmnetwork/zombies/mapeditor/client/TranslationKeys.java
@@ -12,22 +12,15 @@ public final class TranslationKeys {
     public static final String GUI_MAPEDITOR_NEW_ROOM = "gui.mapeditor.new_room";
     public static final String GUI_MAPEDITOR_NEW_WINDOW = "gui.mapeditor.new_window";
     public static final String GUI_MAPEDITOR_NEW_DOOR = "gui.mapeditor.new_door";
-    public static final String GUI_MAPEDITOR_CREATING_MAP = "gui.mapeditor.creating_map";
     public static final String GUI_MAPEDITOR_SAVE = "gui.mapeditor.save";
     public static final String GUI_MAPEDITOR_LOAD = "gui.mapeditor.load";
-    public static final String GUI_MAPEDITOR_EDIT_MAP = "gui.mapeditor.edit_map";
     public static final String GUI_MAPEDITOR_DELETE_MAP = "gui.mapeditor.delete_map";
-    public static final String GUI_MAPEDITOR_DISPLAY_NAME = "gui.mapeditor.display.name";
     public static final String GUI_MAPEDITOR_FEEDBACK_NO_ACTIVE_MAP = "gui.mapeditor.feedback.no_active_map";
     public static final String GUI_MAPEDITOR_FEEDBACK_EMPTY_MAP_NAME = "gui.mapeditor.feedback.empty_map_name";
     public static final String GUI_MAPEDITOR_FEEDBACK_NO_SELECTION = "gui.mapeditor.feedback.no_selection";
-    public static final String GUI_MAPEDITOR_ORIGIN = "gui.mapeditor.origin";
-    public static final String GUI_MAPEDITOR_DISPLAY_ITEM_NBT = "gui.mapeditor.display.item_nbt";
     public static final String GUI_MAPEDITOR_CURRENT_MAP = "gui.mapeditor.current_map";
     public static final String GUI_MAPEDITOR_NO_MAP = "gui.mapeditor.no_map";
     public static final String GUI_MAPEDITOR_FEEDBACK_MAP_NAME_EXISTS = "gui.mapeditor.feedback.map_name_exists";
-    public static final String GUI_MAPEDITOR_FEEDBACK_NO_OVERLAPPING_ROOM =
-            "gui.mapeditor.feedback.no_overlapping_room";
     public static final String GUI_MAPEDITOR_YES = "gui.mapeditor.yes";
     public static final String GUI_MAPEDITOR_NO = "gui.mapeditor.no";
     public static final String GUI_MAPEDITOR_DELETE_MAP_QUERY = "gui.mapeditor.delete_map_query";
