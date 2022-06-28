6f9d0a7778ed0529186b49ce314f0bfd220c0eae
steank <dnd.initiative.tracker@gmail.com>
2022-06-28 09:54:43 -0700

    abusing kotlin's nullable types to allow libraryDirectory to be unset

--- phantazm-server/build.gradle.kts
+++ phantazm-server/build.gradle.kts
@@ -34,7 +34,7 @@ tasks.jar {
     manifest {
         attributes(
             "Class-Path" to copyLibsTask.outputs.files.joinToString(" ") {
-                "libs/${it.relativeTo(copyLibsTask.libraryDirectory).toPath().joinToString("/")}"
+                "libs/${it.relativeTo(copyLibsTask.libraryDirectory!!).toPath().joinToString("/")}"
             },
             "Main-Class" to "com.github.phantazmnetwork.server.PhantazmServer",
             "Multi-Release" to true
