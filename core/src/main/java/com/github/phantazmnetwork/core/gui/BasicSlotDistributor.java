dcbc3bee1fbb34e4cf883005d2e064ed7b7bfa3a
steank <dnd.initiative.tracker@gmail.com>
2022-08-10 17:43:12 -0700

    maybe a working SlotDistributor implementation? (wip)

--- core/src/main/java/com/github/phantazmnetwork/core/gui/BasicSlotDistributor.java
+++ core/src/main/java/com/github/phantazmnetwork/core/gui/BasicSlotDistributor.java
@@ -17,7 +17,7 @@ public class BasicSlotDistributor implements SlotDistributor {
                     "Distributed item count " + itemCount + " cannot be greater than size " + size);
         }
 
-        int totalWidth = computeLength(itemCount, padding);
+        int totalWidth = computeSize(itemCount, padding);
         int rows = MathUtils.ceilDiv(totalWidth, width);
         int rowFactor = Math.max(0, rows - 1);
         int totalHeight = rows + (padding * rowFactor);
@@ -28,20 +28,20 @@ public class BasicSlotDistributor implements SlotDistributor {
             actualPadding = (totalHeight - rows) / rowFactor;
         }
 
-        boolean adjust = canAdjust(actualPadding, rows, height);
-        int columnLength = computeLength(rows, actualPadding);
-        int leftover = height - columnLength;
-        int baseOffset = leftover / 2;
+        int maxRowItems = MathUtils.ceilDiv(width, actualPadding + 1);
 
-        int[] slots = new int[itemCount];
+        boolean adjust = canAdjust(actualPadding, rows, height);
+        int actualHeight = computeSize(rows, actualPadding);
+        int baseOffset = (height - actualHeight) / 2;
         int center = (rows / 2) - 1;
 
-        int maxRowItems = MathUtils.ceilDiv(width, actualPadding + 1);
+        int[] slots = new int[itemCount];
         int slotIndex = 0;
         for (int i = 0; i < rows; i++) {
-            int rowStartIndex = computeSlot(adjust, actualPadding, center, i, baseOffset) * width;
+            int rowStartIndex = applyOffset(adjust, actualPadding, center, i, baseOffset) * width;
             int itemsThisRow = i < rows - 1 ? maxRowItems : itemCount % maxRowItems;
             fillRow(slots, slotIndex, width, actualPadding, rowStartIndex, itemsThisRow);
+            slotIndex += itemsThisRow;
         }
 
         return slots;
@@ -52,23 +52,20 @@ public class BasicSlotDistributor implements SlotDistributor {
         boolean adjust = canAdjust(padding, items, width);
 
         //the actual length of the row, including padding
-        int rowLength = computeLength(items, padding);
-
-        //the number of unused spaces that aren't included in padding
-        int leftover = width - rowLength;
+        int actualWidth = computeSize(items, padding);
 
         //the initial offset to use in order to try and keep the row centered
-        int baseOffset = leftover / 2;
+        int baseOffset = (width - actualWidth) / 2;
 
         int slot = rowStartIndex + baseOffset;
         int center = (items / 2) - 1;
         for (int j = 0; j < items; j++) {
-            slots[slotStartIndex++] = computeSlot(adjust, padding, center, j, slot);
+            slots[slotStartIndex++] = applyOffset(adjust, padding, center, j, slot);
             slot += padding + 1;
         }
     }
 
-    private static int computeSlot(boolean adjust, int padding, int center, int index, int slot) {
+    private static int applyOffset(boolean adjust, int padding, int center, int index, int slot) {
         return (adjust && ((padding > 0) == (index <= center))) ? slot + 1 : slot;
     }
 
@@ -76,7 +73,7 @@ public class BasicSlotDistributor implements SlotDistributor {
         return (padding > 0 || items < size) && ((items % 2 == 0) == (size % 2 != 0));
     }
 
-    private static int computeLength(int items, int padding) {
+    private static int computeSize(int items, int padding) {
         return items + (padding * Math.max(0, items - 1));
     }
 }
