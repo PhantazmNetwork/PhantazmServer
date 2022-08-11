6731a10cbccbce7accd4fbb9e91cb23341073a57
steank <dnd.initiative.tracker@gmail.com>
2022-08-10 17:38:33 -0700

    pain

--- core/src/main/java/com/github/phantazmnetwork/core/gui/BasicSlotDistributor.java
+++ core/src/main/java/com/github/phantazmnetwork/core/gui/BasicSlotDistributor.java
@@ -28,14 +28,28 @@ public class BasicSlotDistributor implements SlotDistributor {
             actualPadding = (totalHeight - rows) / rowFactor;
         }
 
+        boolean adjust = canAdjust(actualPadding, rows, height);
+        int columnLength = computeLength(rows, actualPadding);
+        int leftover = height - columnLength;
+        int baseOffset = leftover / 2;
+
         int[] slots = new int[itemCount];
+        int center = (rows / 2) - 1;
+
+        int maxRowItems = MathUtils.ceilDiv(width, actualPadding + 1);
+        int slotIndex = 0;
+        for (int i = 0; i < rows; i++) {
+            int rowStartIndex = computeSlot(adjust, actualPadding, center, i, baseOffset) * width;
+            int itemsThisRow = i < rows - 1 ? maxRowItems : itemCount % maxRowItems;
+            fillRow(slots, slotIndex, width, actualPadding, rowStartIndex, itemsThisRow);
+        }
 
         return slots;
     }
 
     private static void fillRow(int[] slots, int slotStartIndex, int width, int padding, int rowStartIndex, int items) {
         //true if additional adjustments should be made to necessary slots in order to keep them visually centered
-        boolean centerAdjust = (padding > 0 || items < width) && ((items % 2 == 0) == (width % 2 != 0));
+        boolean adjust = canAdjust(padding, items, width);
 
         //the actual length of the row, including padding
         int rowLength = computeLength(items, padding);
@@ -49,9 +63,17 @@ public class BasicSlotDistributor implements SlotDistributor {
         int slot = rowStartIndex + baseOffset;
         int center = (items / 2) - 1;
         for (int j = 0; j < items; j++) {
-            slots[slotStartIndex++] = (centerAdjust && ((padding > 0) == (j <= center))) ? slot + 1 : slot;
+            slots[slotStartIndex++] = computeSlot(adjust, padding, center, j, slot);
             slot += padding + 1;
         }
+    }
+
+    private static int computeSlot(boolean adjust, int padding, int center, int index, int slot) {
+        return (adjust && ((padding > 0) == (index <= center))) ? slot + 1 : slot;
+    }
+
+    private static boolean canAdjust(int padding, int items, int size) {
+        return (padding > 0 || items < size) && ((items % 2 == 0) == (size % 2 != 0));
     }
 
     private static int computeLength(int items, int padding) {
