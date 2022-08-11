e0ff1172a2b5df75beed50848082e25e0f425970
steank <dnd.initiative.tracker@gmail.com>
2022-08-10 17:57:03 -0700

    committing to show proof that it WORKED on the FIRST TRY

---
+++ core/src/test/java/com/github/phantazmnetwork/core/gui/BasicSlotDistributorTest.java
@@ -0,0 +1,24 @@
+package com.github.phantazmnetwork.core.gui;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicSlotDistributorTest {
+    @Test
+    void test3x3_1Item_padding0() {
+        SlotDistributor slotDistributor = new BasicSlotDistributor(0);
+        int[] slots = slotDistributor.distribute(3, 3, 1);
+        
+        assertArrayEquals(new int[] {4}, slots);
+    }
+
+    @Test
+    void test3x3_2Items_padding0() {
+        SlotDistributor slotDistributor = new BasicSlotDistributor(0);
+        int[] slots = slotDistributor.distribute(3, 3, 2);
+
+        assertEquals(2, slots.length);
+        assertArrayEquals(new int[] {3, 5}, slots);
+    }
+}
