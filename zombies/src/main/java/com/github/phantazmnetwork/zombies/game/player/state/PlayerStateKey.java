2d1ccb8a4a146e717760084e9549a8b012b64933
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2022-10-02 12:54:19 -0400

    changes to conditional sidebar updater
    first commit in almost a month

--- zombies/src/main/java/com/github/phantazmnetwork/zombies/game/player/state/PlayerStateKey.java
+++ zombies/src/main/java/com/github/phantazmnetwork/zombies/game/player/state/PlayerStateKey.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
+@SuppressWarnings("unused") // Generic parameter not technically needed, but nicer for stronger API in ZombiesPlayer
 public record PlayerStateKey<TContext>(@NotNull Key key) implements Keyed {
 
     public PlayerStateKey {
