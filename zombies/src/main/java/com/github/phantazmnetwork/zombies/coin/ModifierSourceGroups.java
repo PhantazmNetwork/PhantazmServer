16066e199b88eb6cf1ddf927a37460d9247708fe
steank <dnd.initiative.tracker@gmail.com>
2022-11-17 03:03:06 -0800

    rename

--- zombies/src/main/java/com/github/phantazmnetwork/zombies/coin/ModifierSourceGroups.java
+++ zombies/src/main/java/com/github/phantazmnetwork/zombies/coin/ModifierSourceGroups.java
@@ -4,5 +4,5 @@ import com.github.phantazmnetwork.commons.Namespaces;
 import net.kyori.adventure.key.Key;
 
 public final class ModifierSourceGroups {
-    public static Key MAP_GOLD = Key.key(Namespaces.PHANTAZM, "zombies.coin_modifier.map_gold");
+    public static Key MOB_GOLD_GAIN = Key.key(Namespaces.PHANTAZM, "zombies.coin_modifier.mob_gold_gain");
 }
