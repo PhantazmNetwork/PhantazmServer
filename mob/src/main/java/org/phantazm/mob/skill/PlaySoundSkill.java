310977e730cd94d60e7d66c9583963edf693331e
steank <dnd.initiative.tracker@gmail.com>
2023-06-05 06:25:00 -0700

    minestom always used sound seed 0? kinda weird but fixed it

--- mob/src/main/java/org/phantazm/mob/skill/PlaySoundSkill.java
+++ mob/src/main/java/org/phantazm/mob/skill/PlaySoundSkill.java
@@ -10,6 +10,7 @@ import org.phantazm.mob.PhantazmMob;
 import org.phantazm.mob.target.TargetSelector;
 
 import java.util.Objects;
+import java.util.Random;
 
 /**
  * A {@link Skill} that plays a {@link Sound}.
@@ -19,6 +20,7 @@ import java.util.Objects;
 public class PlaySoundSkill implements Skill {
     private final Data data;
     private final TargetSelector<Object> selector;
+    private final Random random;
 
     /**
      * Creates a {@link PlaySoundSkill}.
@@ -29,6 +31,7 @@ public class PlaySoundSkill implements Skill {
     public PlaySoundSkill(@NotNull Data data, @NotNull @Child("selector") TargetSelector<Object> selector) {
         this.data = Objects.requireNonNull(data, "data");
         this.selector = Objects.requireNonNull(selector, "selector");
+        this.random = new Random();
     }
 
     @Override
@@ -40,10 +43,10 @@ public class PlaySoundSkill implements Skill {
                     Instance instance = player.getInstance();
                     if (instance != null) {
                         if (data.followAudience) {
-                            instance.playSound(data.sound, player.getPosition());
+                            player.playSound(randomize(), player.getPosition());
                         }
                         else {
-                            instance.playSound(data.sound, self.entity().getPosition());
+                            instance.playSound(randomize(), self.entity().getPosition());
                         }
                     }
                 }
@@ -51,16 +54,20 @@ public class PlaySoundSkill implements Skill {
             else {
                 Audience target = (Audience)object;
                 if (data.followAudience) {
-                    target.playSound(data.sound, Sound.Emitter.self());
+                    target.playSound(randomize(), Sound.Emitter.self());
                 }
                 else {
                     Instance instance = self.entity().getInstance();
                     if (instance != null) {
-                        instance.playSound(data.sound, Sound.Emitter.self());
+                        instance.playSound(randomize(), Sound.Emitter.self());
                     }
                 }
             }
         });
+    }
+
+    private Sound randomize() {
+        return Sound.sound(data.sound).seed(random.nextLong()).build();
     }
 
     @DataObject
