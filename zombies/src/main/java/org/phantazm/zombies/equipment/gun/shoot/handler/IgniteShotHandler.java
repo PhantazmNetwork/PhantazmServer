b2dc8f8d7d2127ade14761c4e2777cfc0fc5bde5
steank <dnd.initiative.tracker@gmail.com>
2023-06-06 14:45:36 -0700

    improve IgniteShotHandler by allowing customization of the damage rate, amount, armor bypass, etc.

--- zombies/src/main/java/org/phantazm/zombies/equipment/gun/shoot/handler/IgniteShotHandler.java
+++ zombies/src/main/java/org/phantazm/zombies/equipment/gun/shoot/handler/IgniteShotHandler.java
@@ -4,16 +4,19 @@ import com.github.steanky.element.core.annotation.Cache;
 import com.github.steanky.element.core.annotation.DataObject;
 import com.github.steanky.element.core.annotation.FactoryMethod;
 import com.github.steanky.element.core.annotation.Model;
+import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Entity;
+import net.minestom.server.entity.LivingEntity;
+import net.minestom.server.entity.damage.DamageType;
+import net.minestom.server.tag.Tag;
 import org.jetbrains.annotations.NotNull;
 import org.phantazm.zombies.equipment.gun.Gun;
 import org.phantazm.zombies.equipment.gun.GunState;
 import org.phantazm.zombies.equipment.gun.shoot.GunHit;
 import org.phantazm.zombies.equipment.gun.shoot.GunShot;
 
-import java.util.Collection;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 /**
  * A {@link ShotHandler} that sets {@link Entity}s on fire.
@@ -24,6 +27,10 @@ public class IgniteShotHandler implements ShotHandler {
 
     private final Data data;
 
+    private final Tag<Boolean> onFire;
+    private final Tag<Long> lastFireDamage;
+    private final Deque<LivingEntity> targets;
+
     /**
      * Creates an {@link IgniteShotHandler}.
      *
@@ -32,32 +39,60 @@ public class IgniteShotHandler implements ShotHandler {
     @FactoryMethod
     public IgniteShotHandler(@NotNull Data data) {
         this.data = Objects.requireNonNull(data, "data");
+        UUID uuid = UUID.randomUUID();
+        this.onFire = Tag.Boolean("on_fire_" + uuid).defaultValue(false);
+        this.lastFireDamage = Tag.Long("on_fire_" + uuid).defaultValue(-1L);
+        this.targets = new ConcurrentLinkedDeque<>();
     }
 
     @Override
     public void handle(@NotNull Gun gun, @NotNull GunState state, @NotNull Entity attacker,
             @NotNull Collection<UUID> previousHits, @NotNull GunShot shot) {
-        for (GunHit target : shot.regularTargets()) {
-            target.entity().setFireForDuration(data.duration());
-        }
-        for (GunHit target : shot.headshotTargets()) {
-            target.entity().setFireForDuration(data.headshotDuration());
+        setFire(shot.regularTargets(), data.normalFireTicks);
+        setFire(shot.headshotTargets(), data.headshotFireTicks);
+    }
+
+    private void setFire(Collection<GunHit> hits, int duration) {
+        for (GunHit target : hits) {
+            LivingEntity entity = target.entity();
+            entity.setFireForDuration(duration);
+            entity.setTag(lastFireDamage, System.currentTimeMillis());
+            targets.add(entity);
         }
     }
 
     @Override
     public void tick(@NotNull GunState state, long time) {
+        targets.removeIf(target -> {
+            if (target.isDead() || target.isRemoved() || !target.isOnFire()) {
+                remove(target);
+                return true;
+            }
 
+            if ((time - target.getTag(this.lastFireDamage)) / MinecraftServer.TICK_MS >= data.damageInterval) {
+                damage(target);
+                target.setTag(this.lastFireDamage, time);
+            }
+
+            return false;
+        });
     }
 
-    /**
-     * Data for an {@link IgniteShotHandler}.
-     *
-     * @param duration         The duration of the fire for regular targets
-     * @param headshotDuration The duration of the fire for headshots
-     */
+    private void remove(LivingEntity target) {
+        target.removeTag(onFire);
+        target.removeTag(lastFireDamage);
+    }
+
+    private void damage(LivingEntity target) {
+        target.damage(DamageType.ON_FIRE, data.damage, data.bypassArmor);
+    }
+
     @DataObject
-    public record Data(int duration, int headshotDuration) {
+    public record Data(int normalFireTicks,
+                       int headshotFireTicks,
+                       int damageInterval,
+                       float damage,
+                       boolean bypassArmor) {
 
     }
 
