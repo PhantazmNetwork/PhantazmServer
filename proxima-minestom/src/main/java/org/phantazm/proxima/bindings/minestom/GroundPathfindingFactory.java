1fbcda4ea2128699f1b94e9952b9af200b166865
steank <dnd.initiative.tracker@gmail.com>
2023-07-06 04:43:03 -0700

    configurable targetDeviation (defaults to 0)

--- proxima-minestom/src/main/java/org/phantazm/proxima/bindings/minestom/GroundPathfindingFactory.java
+++ proxima-minestom/src/main/java/org/phantazm/proxima/bindings/minestom/GroundPathfindingFactory.java
@@ -4,9 +4,14 @@ import com.github.steanky.element.core.annotation.Cache;
 import com.github.steanky.element.core.annotation.DataObject;
 import com.github.steanky.element.core.annotation.FactoryMethod;
 import com.github.steanky.element.core.annotation.Model;
+import com.github.steanky.ethylene.core.ConfigElement;
+import com.github.steanky.ethylene.core.ConfigPrimitive;
+import com.github.steanky.ethylene.mapper.annotation.Default;
 import com.github.steanky.proxima.node.Node;
 import com.github.steanky.proxima.path.Pathfinder;
+import com.github.steanky.vector.Vec3D;
 import com.github.steanky.vector.Vec3I2ObjectMap;
+import com.github.steanky.vector.Vec3IBiPredicate;
 import net.minestom.server.entity.EntityType;
 import org.jetbrains.annotations.NotNull;
 
@@ -26,6 +31,12 @@ public class GroundPathfindingFactory implements Pathfinding.Factory {
     public @NotNull Pathfinding make(@NotNull Pathfinder pathfinder,
             @NotNull ThreadLocal<Vec3I2ObjectMap<Node>> nodeMapLocal, @NotNull InstanceSpaceHandler spaceHandler,
             @NotNull EntityType entityType) {
+        Vec3IBiPredicate predicate = data.targetDeviation <= 0
+                                     ? (x1, y1, z1, x2, y2, z2) -> x1 == x2 && y1 == y2 && z1 == z2
+                                     : (x1, y1, z1, x2, y2, z2) ->
+                                             Vec3D.distanceSquared(x1 + 0.5, y1, z1 + 0.5, x2 + 0.5, y2, z2 + 0.5) <=
+                                                     data.targetDeviation;
+
         return new Pathfinding(pathfinder, nodeMapLocal, spaceHandler, entityType) {
             @Override
             protected float jumpHeight() {
@@ -41,11 +52,19 @@ public class GroundPathfindingFactory implements Pathfinding.Factory {
             protected float stepHeight() {
                 return data.stepHeight;
             }
+
+            @Override
+            protected @NotNull Vec3IBiPredicate successPredicate() {
+                return predicate;
+            }
         };
     }
 
     @DataObject
-    public record Data(float jumpHeight, float fallTolerance, float stepHeight) {
-
+    public record Data(float jumpHeight, float fallTolerance, float stepHeight, double targetDeviation) {
+        @Default("targetDeviation")
+        public static @NotNull ConfigElement defaultTargetDeviation() {
+            return ConfigPrimitive.of(0.0);
+        }
     }
 }
