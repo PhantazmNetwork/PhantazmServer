7742ed2e3b65d8f55ff9d0c0ddd1a2742c3b38fc
steank <dnd.initiative.tracker@gmail.com>
2023-07-22 07:57:34 -0700

    add upgradeFormatMessage to UpgradeEquipmentInteractor

--- zombies/src/main/java/org/phantazm/zombies/map/shop/interactor/UpgradeEquipmentInteractor.java
+++ zombies/src/main/java/org/phantazm/zombies/map/shop/interactor/UpgradeEquipmentInteractor.java
@@ -2,6 +2,11 @@ package org.phantazm.zombies.map.shop.interactor;
 
 import com.github.steanky.element.core.annotation.*;
 import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import net.minestom.server.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.phantazm.core.equipment.Equipment;
 import org.phantazm.core.equipment.Upgradable;
@@ -16,6 +21,7 @@ import java.util.Optional;
 @Model("zombies.map.shop.interactor.upgrade_equipment")
 @Cache(false)
 public class UpgradeEquipmentInteractor implements ShopInteractor {
+    private final Data data;
     private final UpgradePath upgradePath;
     private final List<ShopInteractor> notUpgradableInteractors;
     private final List<ShopInteractor> noHeldEquipmentInteractors;
@@ -23,11 +29,12 @@ public class UpgradeEquipmentInteractor implements ShopInteractor {
     private final List<ShopInteractor> upgradeInteractors;
 
     @FactoryMethod
-    public UpgradeEquipmentInteractor(@NotNull @Child("upgrade_path") UpgradePath upgradePath,
+    public UpgradeEquipmentInteractor(@NotNull Data data, @NotNull @Child("upgrade_path") UpgradePath upgradePath,
             @NotNull @Child("not_upgradable_interactors") List<ShopInteractor> notUpgradableInteractors,
             @NotNull @Child("no_held_equipment_interactors") List<ShopInteractor> noHeldEquipmentInteractors,
             @NotNull @Child("no_upgrade_interactors") List<ShopInteractor> noUpgradeInteractors,
             @NotNull @Child("upgrade_interactors") List<ShopInteractor> upgradeInteractors) {
+        this.data = data;
         this.upgradePath = upgradePath;
         this.notUpgradableInteractors = notUpgradableInteractors;
         this.noHeldEquipmentInteractors = noHeldEquipmentInteractors;
@@ -45,33 +52,42 @@ public class UpgradeEquipmentInteractor implements ShopInteractor {
 
     @Override
     public boolean handleInteraction(@NotNull PlayerInteraction interaction) {
-        return interaction.player().getPlayer().map(player -> {
-            ZombiesPlayer zombiesPlayer = interaction.player();
-            Optional<Equipment> heldEquipmentOptional = zombiesPlayer.getHeldEquipment();
-            if (heldEquipmentOptional.isEmpty()) {
-                ShopInteractor.handle(noHeldEquipmentInteractors, interaction);
-                return false;
-            }
+        ZombiesPlayer zombiesPlayer = interaction.player();
+        Optional<Equipment> heldEquipmentOptional = zombiesPlayer.getHeldEquipment();
+        if (heldEquipmentOptional.isEmpty()) {
+            ShopInteractor.handle(noHeldEquipmentInteractors, interaction);
+            return false;
+        }
 
-            Equipment equipment = heldEquipmentOptional.get();
+        Equipment equipment = heldEquipmentOptional.get();
 
-            Optional<Key> upgradeKeyOptional = upgradePath.nextUpgrade(equipment.key());
-            if (!(equipment instanceof Upgradable upgradable) || (upgradeKeyOptional.isPresent() &&
-                    !upgradable.getSuggestedUpgrades().contains(upgradeKeyOptional.get()))) {
-                ShopInteractor.handle(notUpgradableInteractors, interaction);
-                return false;
-            }
+        Optional<Key> upgradeKeyOptional = upgradePath.nextUpgrade(equipment.key());
+        if (!(equipment instanceof Upgradable upgradable) || (upgradeKeyOptional.isPresent() &&
+                !upgradable.getSuggestedUpgrades().contains(upgradeKeyOptional.get()))) {
+            ShopInteractor.handle(notUpgradableInteractors, interaction);
+            return false;
+        }
 
-            if (upgradeKeyOptional.isEmpty()) {
-                ShopInteractor.handle(noUpgradeInteractors, interaction);
-                return false;
-            }
+        if (upgradeKeyOptional.isEmpty()) {
+            ShopInteractor.handle(noUpgradeInteractors, interaction);
+            return false;
+        }
 
-            Key upgradeKey = upgradeKeyOptional.get();
-            upgradable.setLevel(upgradeKey);
-            ShopInteractor.handle(upgradeInteractors, interaction);
-            return true;
-        }).orElse(false);
+        Key upgradeKey = upgradeKeyOptional.get();
+        upgradable.setLevel(upgradeKey);
+
+        ItemStack itemStack = equipment.getItemStack();
+        Component component = itemStack.getDisplayName();
+        if (component == null) {
+            component = Component.translatable(itemStack.material().registry().translationKey());
+        }
+
+        TagResolver tag = Placeholder.component("upgraded_item", component);
+        zombiesPlayer.getPlayer().ifPresent(
+                player -> player.sendMessage(MiniMessage.miniMessage().deserialize(data.upgradeFormatMessage, tag)));
+
+        ShopInteractor.handle(upgradeInteractors, interaction);
+        return true;
     }
 
     @Override
@@ -83,7 +99,8 @@ public class UpgradeEquipmentInteractor implements ShopInteractor {
     }
 
     @DataObject
-    public record Data(@NotNull @ChildPath("upgrade_path") String upgradePath,
+    public record Data(@NotNull String upgradeFormatMessage,
+                       @NotNull @ChildPath("upgrade_path") String upgradePath,
                        @NotNull @ChildPath("not_upgradable_interactors") List<String> notUpgradableInteractors,
                        @NotNull @ChildPath("no_held_equipment_interactors") List<String> noHeldEquipmentInteractors,
                        @NotNull @ChildPath("no_upgrade_interactors") List<String> noUpgradeInteractors,
