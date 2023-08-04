56c6362ec903b44fc0ae077417ce26268d46bf25
steank <dnd.initiative.tracker@gmail.com>
2023-08-04 00:53:19 -0700

    default followAudience to false for PlaySoundSkill

--- mob/src/main/java/org/phantazm/mob/skill/PlaySoundSkill.java
+++ mob/src/main/java/org/phantazm/mob/skill/PlaySoundSkill.java
@@ -1,6 +1,9 @@
 package org.phantazm.mob.skill;
 
 import com.github.steanky.element.core.annotation.*;
+import com.github.steanky.ethylene.core.ConfigElement;
+import com.github.steanky.ethylene.core.ConfigPrimitive;
+import com.github.steanky.ethylene.mapper.annotation.Default;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.sound.Sound;
 import net.minestom.server.entity.Player;
@@ -38,8 +41,11 @@ public class PlaySoundSkill implements Skill {
     public void use(@NotNull PhantazmMob self) {
         selector.selectTarget(self).ifPresent(object -> {
             if (object instanceof Iterable<?> iterable) {
-                Iterable<Player> audienceIterable = (Iterable<Player>)iterable;
-                for (Player player : audienceIterable) {
+                for (Object playerObject : iterable) {
+                    if (!(playerObject instanceof Player player)) {
+                        continue;
+                    }
+
                     Instance instance = player.getInstance();
                     if (instance != null) {
                         if (data.followAudience) {
@@ -72,12 +78,9 @@ public class PlaySoundSkill implements Skill {
 
     @DataObject
     public record Data(@NotNull @ChildPath("selector") String selector, @NotNull Sound sound, boolean followAudience) {
-
-        public Data {
-            Objects.requireNonNull(selector, "selector");
-            Objects.requireNonNull(sound, "sound");
+        @Default("followAudience")
+        public static @NotNull ConfigElement defaultFollowAudience() {
+            return ConfigPrimitive.of(false);
         }
-
     }
-
 }
