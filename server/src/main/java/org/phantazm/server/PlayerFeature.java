db42ec7bdaa46e793e61211e72188a6b1cd7d94a
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2023-08-10 15:00:59 -0400

    troll the optifine users

--- server/src/main/java/org/phantazm/server/PlayerFeature.java
+++ server/src/main/java/org/phantazm/server/PlayerFeature.java
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.event.player.PlayerPluginMessageEvent;
 import net.minestom.server.event.player.PlayerSpawnEvent;
 import org.jetbrains.annotations.NotNull;
 import org.phantazm.server.config.player.PlayerConfig;
@@ -28,6 +29,11 @@ public final class PlayerFeature {
             event.getPlayer().setCustomNameVisible(true);
 
             event.getPlayer().sendMessage(playerConfig.joinMessage());
+        });
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerPluginMessageEvent.class, event -> {
+            if (event.getIdentifier().equals("minecraft:brand") && event.getMessageString().contains("optifine")) {
+                event.getPlayer().sendMessage(playerConfig.optifineMessage());
+            }
         });
     }
 
