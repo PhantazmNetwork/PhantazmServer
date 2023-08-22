eae2ababe216a79951b89457003484695b254d43
steank <dnd.initiative.tracker@gmail.com>
2023-08-22 06:15:05 -0700

    add defaults for MobData

--- mob/src/main/java/org/phantazm/mob2/MobData.java
+++ mob/src/main/java/org/phantazm/mob2/MobData.java
@@ -1,6 +1,10 @@
 package org.phantazm.mob2;
 
+import com.github.steanky.ethylene.core.ConfigElement;
+import com.github.steanky.ethylene.core.ConfigPrimitive;
+import com.github.steanky.ethylene.core.collection.ConfigList;
 import com.github.steanky.ethylene.core.collection.ConfigNode;
+import com.github.steanky.ethylene.mapper.annotation.Default;
 import it.unimi.dsi.fastutil.objects.Object2FloatMap;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.key.Keyed;
@@ -9,7 +13,6 @@ import net.minestom.server.entity.EquipmentSlot;
 import net.minestom.server.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.Unmodifiable;
 
 import java.util.List;
 import java.util.Map;
@@ -21,5 +24,34 @@ public record MobData(@NotNull Key key,
     @Nullable MobMeta meta,
     @NotNull List<ConfigNode> skills,
     @NotNull List<ConfigNode> goals,
-    @NotNull @Unmodifiable ConfigNode extra) implements Keyed {
+    @NotNull ConfigNode extra) implements Keyed {
+    @Default("equipment")
+    public static @NotNull ConfigElement defaultEquipment() {
+        return ConfigNode.of();
+    }
+
+    @Default("attributes")
+    public static @NotNull ConfigElement defaultAttributes() {
+        return ConfigNode.of();
+    }
+
+    @Default("meta")
+    public static @NotNull ConfigElement defaultMeta() {
+        return ConfigPrimitive.NULL;
+    }
+
+    @Default("skills")
+    public static @NotNull ConfigElement defaultSkills() {
+        return ConfigList.of();
+    }
+
+    @Default("goals")
+    public static @NotNull ConfigElement defaultGoals() {
+        return ConfigList.of();
+    }
+
+    @Default("extra")
+    public static @NotNull ConfigElement defaultExtra() {
+        return ConfigNode.of();
+    }
 }
