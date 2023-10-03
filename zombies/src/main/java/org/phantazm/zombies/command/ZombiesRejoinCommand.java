29285d49f93af82e6a741c86fdf4b5517a3c6d26
steank <dnd.initiative.tracker@gmail.com>
2023-10-03 10:36:37 -0700

    slight fix for suggestion feedback

--- zombies/src/main/java/org/phantazm/zombies/command/ZombiesRejoinCommand.java
+++ zombies/src/main/java/org/phantazm/zombies/command/ZombiesRejoinCommand.java
@@ -42,7 +42,7 @@ public class ZombiesRejoinCommand extends Command {
                 }
 
                 Stage stage = zombiesScene.currentStage();
-                if (stage != null && !stage.canRejoin()) {
+                if (stage == null || !stage.canRejoin()) {
                     continue;
                 }
 
