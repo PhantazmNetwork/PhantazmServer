be7db36e287207ce7b26c443d7d80bd761df541c
steank <dnd.initiative.tracker@gmail.com>
2023-10-08 03:19:13 -0700

    zombies game joining takes modifiers into account

--- zombies/src/main/java/org/phantazm/zombies/scene2/JoinZombiesMap.java
+++ zombies/src/main/java/org/phantazm/zombies/scene2/JoinZombiesMap.java
@@ -4,8 +4,6 @@ import net.kyori.adventure.key.Key;
 import org.jetbrains.annotations.NotNull;
 import org.phantazm.core.player.PlayerView;
 import org.phantazm.core.scene2.SceneCreator;
-import org.phantazm.core.scene2.SceneManager;
-import org.phantazm.zombies.modifier.ModifierHandler;
 
 import java.util.Collection;
 import java.util.Objects;
@@ -13,22 +11,12 @@ import java.util.Set;
 
 public class JoinZombiesMap extends JoinZombies {
     private final Key mapKey;
-    private final Set<Key> modifiers;
 
     public JoinZombiesMap(@NotNull Collection<@NotNull PlayerView> players,
         @NotNull SceneCreator<ZombiesScene> sceneCreator, @NotNull Key mapKey,
         @NotNull Set<Key> modifiers) {
-        super(players, sceneCreator);
+        super(players, sceneCreator, modifiers);
         this.mapKey = Objects.requireNonNull(mapKey);
-        this.modifiers = Objects.requireNonNull(modifiers);
-    }
-
-    @Override
-    public @NotNull ZombiesScene createNewScene(@NotNull SceneManager manager) {
-        ZombiesScene scene = super.createNewScene(manager);
-        ModifierHandler.Global.instance().applyModifiers(modifiers, scene);
-
-        return scene;
     }
 
     @Override
