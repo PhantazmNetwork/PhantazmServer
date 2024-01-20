4e5b41b47cb62a2ba207b46d006241802c4b11a0
steank <dnd.initiative.tracker@gmail.com>
2024-01-20 04:03:39 -0800

    don't force usage of duration-based fire for LivingEntity, ApplyFireSkill tracks which skill actually set the mob on fire

--- mob/src/main/java/org/phantazm/mob2/skill/ApplyFireSkill.java
+++ mob/src/main/java/org/phantazm/mob2/skill/ApplyFireSkill.java
@@ -5,9 +5,11 @@ import com.github.steanky.ethylene.core.ConfigElement;
 import com.github.steanky.ethylene.core.ConfigPrimitive;
 import com.github.steanky.ethylene.mapper.annotation.Default;
 import net.minestom.server.entity.Entity;
+import net.minestom.server.tag.Tag;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.phantazm.commons.InjectionStore;
+import org.phantazm.core.TagUtils;
 import org.phantazm.mob2.Mob;
 import org.phantazm.mob2.Target;
 import org.phantazm.mob2.Trigger;
@@ -15,6 +17,7 @@ import org.phantazm.mob2.selector.Selector;
 import org.phantazm.mob2.selector.SelectorComponent;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Model("mob.skill.apply_fire")
 @Cache
@@ -45,20 +48,28 @@ public class ApplyFireSkill implements SkillComponent {
     }
 
     private static final class Impl extends TargetedSkill {
+        private static final Tag<Integer> FIRE_ID_TAG = Tag.Integer(TagUtils.uniqueTagName());
+        private static final AtomicInteger FIRE_ID = new AtomicInteger();
+
         private final Data data;
         private final Set<Entity> entities;
+        private final int fireId;
 
         public Impl(@NotNull Mob self, @NotNull Selector selector, @NotNull Data data) {
             super(self, selector);
             this.entities = Collections.newSetFromMap(new WeakHashMap<>());
             this.data = data;
+
+            this.fireId = FIRE_ID.getAndIncrement();
         }
 
         @Override
         protected void useOnTarget(@NotNull Target target) {
             self.getAcquirable().sync(self -> {
                 target.forType(Entity.class, entity -> {
+                    entity.setTag(FIRE_ID_TAG, fireId);
                     entity.setOnFire(true);
+
                     entities.add(entity);
                 });
             });
@@ -69,7 +80,7 @@ public class ApplyFireSkill implements SkillComponent {
             self.getAcquirable().sync(self -> {
                 for (Entity entity : entities) {
                     entity.getAcquirable().sync(target -> {
-                        if (!target.isRemoved()) {
+                        if (!target.isRemoved() && entity.getTag(FIRE_ID_TAG) == fireId) {
                             target.setOnFire(false);
                         }
                     });
