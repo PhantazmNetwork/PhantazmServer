18639c2549d6dd25e0af5af1a52801035bdb20b9
steank <dnd.initiative.tracker@gmail.com>
2024-01-21 05:36:02 -0800

    intelliJ?

--- commons/src/main/java/org/phantazm/commons/MonoComponent.java
+++
@@ -1,8 +0,0 @@
-package org.phantazm.commons;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.util.function.Function;
-
-public interface MonoComponent<T> extends Function<@NotNull InjectionStore, @NotNull T> {
-}
