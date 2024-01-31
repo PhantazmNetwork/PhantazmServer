5775076ce19fe466a878df765c3f7466c781f850
steank <dnd.initiative.tracker@gmail.com>
2024-01-31 01:25:56 -0800

    console now rats out potential fly hackers and their actual coordinates

--- zombies/src/main/java/org/phantazm/zombies/spawn/BasicSpawnDistributor.java
+++ zombies/src/main/java/org/phantazm/zombies/spawn/BasicSpawnDistributor.java
@@ -2,6 +2,9 @@ package org.phantazm.zombies.spawn;
 
 import it.unimi.dsi.fastutil.Pair;
 import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.Component;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.command.ConsoleSender;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -14,10 +17,15 @@ import org.phantazm.zombies.scene2.ZombiesScene;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
 public class BasicSpawnDistributor implements SpawnDistributor {
+    private static final Object REPORT_LOCK = new Object();
+
     private static final Logger LOGGER = LoggerFactory.getLogger(BasicSpawnDistributor.class);
 
     private final MobSpawner spawner;
@@ -97,6 +105,7 @@ public class BasicSpawnDistributor implements SpawnDistributor {
 
         List<Mob> spawnedMobs = new ArrayList<>(spawnList.size());
         int candidateIndex = 0;
+        boolean reportedThisEntry = false;
         for (int i = spawnList.size() - 1; i >= 0; i--) {
             Pair<Key, Key> spawnEntry = spawnList.get(i);
             Key spawnIdentifier = spawnEntry.first();
@@ -115,8 +124,60 @@ public class BasicSpawnDistributor implements SpawnDistributor {
                 }
             }
 
-            if (!spawned) {
-                LOGGER.warn("Found no suitable spawnpoint for mob {} using spawn type {}", spawnIdentifier, spawnType);
+            if (!spawned && !reportedThisEntry) {
+                Key mapId = zombiesScene.mapSettingsInfo().id();
+
+                List<CompletableFuture<Pair<Component, Reference<ZombiesPlayer>>>> nameFutures =
+                    new ArrayList<>(players.size());
+                for (ZombiesPlayer player : players) {
+                    //probably unnecessary, but long-running name resolutions would otherwise keep the player (and by
+                    //extension the expensive scene object) alive
+                    Reference<ZombiesPlayer> playerReference = new WeakReference<>(player);
+
+                    nameFutures.add(player.module().getPlayerView().getDisplayName().thenApply(component ->
+                        Pair.of(component, playerReference)));
+                }
+
+                @SuppressWarnings("unchecked")
+                CompletableFuture<Pair<Component, Reference<ZombiesPlayer>>>[] completableFutures = nameFutures
+                    .toArray(CompletableFuture[]::new);
+
+                CompletableFuture.allOf(completableFutures).thenRun(() -> {
+                    //global report lock: otherwise lines could get mixed up from different games, confusing evidence
+                    synchronized (REPORT_LOCK) {
+                        LOGGER.warn("Found no suitable spawnpoint for mob {} using spawn type {} on map {}",
+                            spawnIdentifier, spawnType, mapId);
+
+                        LOGGER.warn("Participating players:");
+                        for (CompletableFuture<Pair<Component, Reference<ZombiesPlayer>>> componentFuture :
+                            completableFutures) {
+                            Pair<Component, Reference<ZombiesPlayer>> participant = componentFuture.join();
+                            Component name = participant.first();
+                            ZombiesPlayer zombiesPlayer = participant.second().get();
+
+                            ConsoleSender consoleSender = MinecraftServer.getCommandManager().getConsoleSender();
+                            if (zombiesPlayer == null || !zombiesPlayer.isInGame()) {
+                                consoleSender.sendMessage(name.append(Component.text(" (not in game)")));
+                                continue;
+                            }
+
+                            Optional<Player> player = zombiesPlayer.getPlayer();
+                            if (player.isEmpty()) {
+                                consoleSender.sendMessage(name.append(Component.text(" (not on server)")));
+                                continue;
+                            }
+
+                            Player actualPlayer = player.get();
+                            consoleSender.sendMessage(name.append(Component.text(" @" +
+                                actualPlayer.getPosition())));
+                            if (!zombiesPlayer.isInGame()) {
+                                consoleSender.sendMessage("(above player left game, location may not be accurate)");
+                            }
+                        }
+                    }
+                });
+
+                reportedThisEntry = true;
             }
         }
 
