166e054c8f40fc56cc28e04e707cf7e1338fc08c
MyLadyMalady <cobrinthine.amaranthine@gmail.com>
2024-05-22 10:06:36 +0100

    Added interface methods for friends DB

---
+++ stats/src/main/java/org/phantazm/stats/general/FriendDatabase.java
@@ -0,0 +1,22 @@
+package org.phantazm.stats.general;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+public interface FriendDatabase {
+    record FriendEntry(UUID player, long time) {}
+
+    @NotNull CompletableFuture<Void> initTables();
+
+    @NotNull CompletableFuture<Set<UUID>> fetchNFriends(@NotNull UUID requester, int numberOfRows, int offset);
+
+    // unused - use only for stat API
+    @NotNull CompletableFuture<Set<FriendEntry>> fetchNFriendsWithTime(@NotNull UUID requester, int numberOfRows, int offset);
+
+    @NotNull CompletableFuture<Void> submitFriend(@NotNull UUID player1, @NotNull UUID player2, long timeCreated);
+
+    @NotNull CompletableFuture<Boolean> deleteFriend(@NotNull UUID player1, @NotNull UUID player2);
+}
