eb6e02419ba824fa4a0a2b03f22a26f7c40bd127
steank <dnd.initiative.tracker@gmail.com>
2024-05-22 03:16:38 -0700

    Add SpawnPowerupInteractor

---
+++ zombies/src/main/java/org/phantazm/zombies/map/shop/interactor/SpawnPowerupInteractor.java
@@ -0,0 +1,51 @@
+package org.phantazm.zombies.map.shop.interactor;
+
+import com.github.steanky.element.core.annotation.*;
+import com.github.steanky.vector.Vec3I;
+import net.kyori.adventure.key.Key;
+import net.minestom.server.coordinate.Point;
+import org.jetbrains.annotations.NotNull;
+import org.phantazm.zombies.map.shop.PlayerInteraction;
+import org.phantazm.zombies.map.shop.Shop;
+import org.phantazm.zombies.powerup.PowerupHandler;
+import org.phantazm.zombies.scene2.ZombiesScene;
+
+import java.util.function.Supplier;
+
+@Model("zombies.map.shop.interactor.spawn_powerup")
+@Cache(false)
+public class SpawnPowerupInteractor implements ShopInteractor {
+    private final Data data;
+    private final Supplier<ZombiesScene> zombiesScene;
+
+    private Shop shop;
+
+    @FactoryMethod
+    public SpawnPowerupInteractor(@NotNull Data data, @NotNull Supplier<ZombiesScene> zombiesScene) {
+        this.data = data;
+        this.zombiesScene = zombiesScene;
+    }
+
+    @Override
+    public boolean handleInteraction(@NotNull PlayerInteraction interaction) {
+        ZombiesScene scene = zombiesScene.get();
+        PowerupHandler handler = scene.map().powerupHandler();
+        if (!handler.canSpawnType(data.powerup)) {
+            return false;
+        }
+
+        Point target = shop.mapOrigin().add(data.target.x(), data.target.y(), data.target.z());
+        handler.spawn(data.powerup, target);
+        return true;
+    }
+
+    @Override
+    public void initialize(@NotNull Shop shop) {
+        this.shop = shop;
+    }
+
+    @DataObject
+    public record Data(@NotNull Key powerup,
+        @NotNull Vec3I target) {
+    }
+}
