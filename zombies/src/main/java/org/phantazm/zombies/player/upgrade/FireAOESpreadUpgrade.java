80f258968663d3b348e1398acae056404eff3ec9
steank <dnd.initiative.tracker@gmail.com>
2024-07-17 12:32:49 -0700

    fix: FireAOESpreadUpgrade is now triggered by gun shots rather than just igniting entities

--- zombies/src/main/java/org/phantazm/zombies/player/upgrade/FireAOESpreadUpgrade.java
+++ zombies/src/main/java/org/phantazm/zombies/player/upgrade/FireAOESpreadUpgrade.java
@@ -4,15 +4,16 @@ import com.github.steanky.element.core.annotation.Cache;
 import com.github.steanky.element.core.annotation.DataObject;
 import com.github.steanky.element.core.annotation.FactoryMethod;
 import com.github.steanky.element.core.annotation.Model;
+import net.minestom.server.entity.Entity;
 import net.minestom.server.event.EventListener;
 import net.minestom.server.instance.EntityTracker;
-import net.minestom.server.tag.Tag;
 import org.jetbrains.annotations.NotNull;
 import org.phantazm.commons.InjectionStore;
-import org.phantazm.core.TagUtils;
 import org.phantazm.core.tick.Activable;
 import org.phantazm.mob2.Mob;
-import org.phantazm.zombies.event.player.ZombiesPlayerIgniteTargetEvent;
+import org.phantazm.zombies.equipment.gun.shoot.GunHit;
+import org.phantazm.zombies.equipment.perk.effect.shot.ApplyFireShotEffect;
+import org.phantazm.zombies.event.equipment.EntitiesHitByGunEvent;
 import org.phantazm.zombies.player.ZombiesPlayer;
 
 @Model("zombies.upgrade.fire_aoe_spread")
@@ -33,9 +34,8 @@ public class FireAOESpreadUpgrade implements PlayerUpgradeComponent {
     private static class Internal extends GuardedPlayerUpgrade {
         private Internal(ZombiesPlayer zombiesPlayer, Data data) {
             super(Activable.threadsafeWrapper(new Activable() {
-                private final EventListener<ZombiesPlayerIgniteTargetEvent> event = EventListener.builder(ZombiesPlayerIgniteTargetEvent.class)
-                    .handler(this::handleMobIgnite).build();
-                private static final Tag<Boolean> AOE_IGNITED = Tag.Boolean(TagUtils.uniqueTagName()).defaultValue(false);
+                private final EventListener<EntitiesHitByGunEvent> event = EventListener.builder(EntitiesHitByGunEvent.class)
+                    .handler(this::handleMobShot).build();
 
                 @Override
                 public void start() {
@@ -47,21 +47,27 @@ public class FireAOESpreadUpgrade implements PlayerUpgradeComponent {
                     zombiesPlayer.getScene().sceneNode().removeListener(event);
                 }
 
-                private void handleMobIgnite(ZombiesPlayerIgniteTargetEvent event) {
-                    if (event.getZombiesPlayer() != zombiesPlayer || event.target().getTag(AOE_IGNITED)) {
+                private void handleMobShot(EntitiesHitByGunEvent event) {
+                    if (!event.getShooter().getUuid().equals(zombiesPlayer.getUUID())) {
                         return;
                     }
 
-                    zombiesPlayer.getScene().instance().getEntityTracker().nearbyEntities(event.target().getPosition(),
-                        data.radius, EntityTracker.Target.LIVING_ENTITIES, entity -> {
-                            if (entity == event.target() || !(entity instanceof Mob mob) || mob.getTag(AOE_IGNITED)) {
-                                return;
-                            }
+                    ApplyFireShotEffect applyFire = zombiesPlayer.lookupShotEffect(ApplyFireShotEffect.class);
+                    if (applyFire == null) {
+                        return;
+                    }
 
-                            mob.setTag(AOE_IGNITED, true);
-                            event.cause().perform(mob, zombiesPlayer);
-                            mob.removeTag(AOE_IGNITED);
-                        });
+                    for (GunHit hit : event.targets()) {
+                        Entity hitEntity = hit.entity();
+                        zombiesPlayer.getScene().instance().getEntityTracker().nearbyEntities(hitEntity.getPosition(),
+                            data.radius, EntityTracker.Target.LIVING_ENTITIES, target -> {
+                                if (target == event.getShooter() || target == hitEntity || !(target instanceof Mob)) {
+                                    return;
+                                }
+
+                                applyFire.perform(target, zombiesPlayer);
+                            });
+                    }
                 }
             }), zombiesPlayer);
         }
