7505d82ebf3df28b2cef433a1fa8e122d83a9f95
romqrocha <rodrigo@vancouver4us.com>
2024-07-21 20:56:25 -0700

    Added commands and reformatting cause bored

--- zombies/src/main/java/org/phantazm/zombies/player/upgrade/ApplyLimitedAttributeOnShot.java
+++ zombies/src/main/java/org/phantazm/zombies/player/upgrade/ApplyLimitedAttributeOnShot.java
@@ -21,32 +21,56 @@ import org.phantazm.zombies.player.ZombiesPlayer;
 
 import java.util.*;
 
+/**
+ * Developed for the Frozen Bullets and Quick Fire synergy. When a player
+ * shoots a zombie, this class applies a modifier that can stack up to a
+ * limited level and where each level expires after some time.
+ */
 @Model("zombies.upgrade.apply_limited_attribute_on_shot")
 @Cache
 public class ApplyLimitedAttributeOnShot implements PlayerUpgradeComponent {
+
+    /** A record of configurable data fields for this upgrade. */
     private final Data data;
 
+    /** I think this might make sure that the data fields aren't null */
     @FactoryMethod
     public ApplyLimitedAttributeOnShot(@NotNull Data data) {
         this.data = Objects.requireNonNull(data);
     }
-
     @Override
     public @NotNull PlayerUpgrade apply(@NotNull InjectionStore injectionStore, @NotNull ZombiesPlayer zombiesPlayer) {
         return new Internal(zombiesPlayer, data);
     }
 
     private static final class Internal extends GuardedPlayerUpgrade {
+        @Override
+        public boolean needsTicking() {
+            return true;
+        }
+
+        /**
+         * Private constructor.
+         * @param zombiesPlayer the player who owns this upgrade
+         * @param data a record of configurable data fields for this upgrade
+         */
         private Internal(ZombiesPlayer zombiesPlayer, Data data) {
             super(Activable.threadsafeWrapper(new Activable() {
+                /** Builds event listener and sets handler. */
                 private final EventListener<EntityDamageByGunEvent> listener = EventListener
-                    .builder(EntityDamageByGunEvent.class).handler(this::onShotByGun).build();
+                    .builder(EntityDamageByGunEvent.class).handler(this::onEntityShot).build();
 
+                /** This upgrade's UUID for representing it as an AttributeModifier. */
                 private final UUID uuid = UUID.randomUUID();
 
-                private int attributeLevel;
+                /** Queue for storing the scheduled times of subtracting numberOfStacks. */
+                private final Deque<Integer> schedule = new ArrayDeque<>();
+
+                /** How many stacks of this upgrade's attribute modifier the player has. */
+                private int numberOfStacks;
+
+                /** How many ticks passed since the player got this upgrade. */
                 private int tickTimer;
-                private final Deque<Integer> scheduler = new ArrayDeque<>();
 
                 @Override
                 public void start() {
@@ -58,70 +82,90 @@ public class ApplyLimitedAttributeOnShot implements PlayerUpgradeComponent {
                     zombiesPlayer.getScene().sceneNode().removeListener(listener);
                 }
 
-                private void onShotByGun(EntityDamageByGunEvent event) {
-                    if (!event.getShooter().getUuid().equals(zombiesPlayer.getUUID())) {
-                        // shooter is not the perk owner
-                        return;
-                    }
-
-                    Entity entity = event.getEntity();
-                    if (entity instanceof Mob && resistsSlowDown((Mob) entity)) {
-                        // entity resisted the effect
-                        return;
-                    } else if (attributeLevel <= data.maxAttributeLevel * 2){
-                        attributeLevel++;
-                        scheduler.add(tickTimer + data.attributeDuration);
-                    }
-                }
-
                 @Override
                 public void tick(long time) {
                     tickTimer++;
 
-                    if (attributeLevel > 0) {
-                        while (!scheduler.isEmpty() && tickTimer >= scheduler.peek()) {
-                            scheduler.pop();
-                            attributeLevel--;
+                    if (numberOfStacks > 0) {
+                        while (!schedule.isEmpty() && tickTimer >= schedule.peek()) {
+                            schedule.pop();
+                            numberOfStacks--;
                         }
-
-                        applyAttributeLevel();
+                        applyModifierLevel();
                     }
                 }
 
-                private static boolean resistsSlowDown(Mob mob) {
-                    return mob.data().extra().getBooleanOrDefault(
-                        ExtraNodeKeys.RESIST_SLOW_DOWN, false);
+                /**
+                 * Called every time an entity receives damage from a gun.
+                 * Increments numberOfStacks and schedules it to be decremented
+                 * after a certain number of ticks.
+                 */
+                private void onEntityShot(EntityDamageByGunEvent event) {
+
+                    Entity shooter = event.getShooter();
+                    Entity target = event.getEntity();
+
+                    if (shooter.getUuid().equals(zombiesPlayer.getUUID()) &&
+                        numberOfStacks < data.maxStoredStacks &&
+                        !targetIsResistant(target))
+                    {
+                        schedule.add(tickTimer + data.attributeDuration);
+                        numberOfStacks++;
+                    }
                 }
 
-                private void applyAttributeLevel() {
-                    int actualAttributeLevel = Math.min(attributeLevel, data.maxAttributeLevel);
+                /**
+                 * Returns true if the target resists the debuff relevant to this upgrade.
+                 * (e.g. the target has RESIST_SLOW_DOWN)
+                 * FIXME: hardcoded to only consider frozen bullets resistance
+                 * @param target the entity being damaged
+                 */
+                private boolean targetIsResistant(Entity target) {
+                    if (target instanceof Mob mob) {
+                        return mob.data().extra().getBooleanOrDefault(
+                            ExtraNodeKeys.RESIST_SLOW_DOWN, false);
+                    } else {
+                        return false;
+                    }
+                }
 
+                /**
+                 * Updates the attribute relevant to this upgrade by applying
+                 * the correct modifier level.
+                 */
+                private void applyModifierLevel() {
+                    // remove outdated modifier
                     Optional<Player> playerOptional = zombiesPlayer.getPlayer();
                     if (playerOptional.isEmpty()) {
                         return;
                     }
-
                     AttributeInstance playerAttribute = playerOptional.get().getAttribute(Attributes.get(data.attribute));
-
                     playerAttribute.removeModifier(uuid);
-                    AttributeModifier modifier = new AttributeModifier(uuid, uuid.toString(), data.value * actualAttributeLevel, data.operation);
 
+                    // add updated modifier
+                    int modifierLevel = Math.min(numberOfStacks, data.maxEffectiveStacks);
+                    AttributeModifier modifier = new AttributeModifier(uuid, uuid.toString(), data.value * modifierLevel, data.operation);
                     playerAttribute.addModifier(modifier);
                 }
 
             }), zombiesPlayer);
         }
-
-        @Override
-        public boolean needsTicking() {
-            return true;
-        }
     }
 
+    /**
+     * Record constructor.
+     * @param attribute Minecraft attribute that should be modified
+     * @param maxEffectiveStacks Max number of stacks before the attribute modifier can't go any higher
+     * @param maxStoredStacks Max number of stacks stored before we stop keeping track
+     * @param attributeDuration How long one stack of the attribute modifier lasts before expiring
+     * @param value How much to modify the attribute by
+     * @param operation How to modify the attribute
+     */
     @DataObject
     public record Data(
         @NotNull String attribute,
-        int maxAttributeLevel,
+        int maxEffectiveStacks,
+        int maxStoredStacks,
         int attributeDuration,
         double value,
         @NotNull AttributeOperation operation) {
