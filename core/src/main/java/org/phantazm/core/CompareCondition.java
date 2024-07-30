7541f0ba967305c40bc287c5c86ba6b80812a490
steank <dnd.initiative.tracker@gmail.com>
2024-07-30 11:09:07 -0700

    feat: add additional enum constants to CompareCondition, thanks for the code duplication Cob

--- core/src/main/java/org/phantazm/core/CompareCondition.java
+++ core/src/main/java/org/phantazm/core/CompareCondition.java
@@ -4,56 +4,71 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
+@SuppressWarnings("DuplicatedCode")
 public enum CompareCondition {
     LESS_THAN,
+    LESS_THAN_OR_EQUAL_TO,
     EQUAL_TO,
-    GREATER_THAN;
+    GREATER_THAN,
+    GREATER_THAN_OR_EQUAL_TO;
 
     public boolean compare(double first, double second) {
         return switch (this) {
             case LESS_THAN -> first < second;
+            case LESS_THAN_OR_EQUAL_TO -> first <= second;
             case EQUAL_TO -> first == second;
             case GREATER_THAN -> first > second;
+            case GREATER_THAN_OR_EQUAL_TO -> first >= second;
         };
     }
 
     public boolean compare(float first, float second) {
         return switch (this) {
             case LESS_THAN -> first < second;
+            case LESS_THAN_OR_EQUAL_TO -> first <= second;
             case EQUAL_TO -> first == second;
             case GREATER_THAN -> first > second;
+            case GREATER_THAN_OR_EQUAL_TO -> first >= second;
         };
     }
 
     public boolean compare(long first, long second) {
         return switch (this) {
             case LESS_THAN -> first < second;
+            case LESS_THAN_OR_EQUAL_TO -> first <= second;
             case EQUAL_TO -> first == second;
             case GREATER_THAN -> first > second;
+            case GREATER_THAN_OR_EQUAL_TO -> first >= second;
         };
     }
 
     public boolean compare(int first, int second) {
         return switch (this) {
             case LESS_THAN -> first < second;
+            case LESS_THAN_OR_EQUAL_TO -> first <= second;
             case EQUAL_TO -> first == second;
             case GREATER_THAN -> first > second;
+            case GREATER_THAN_OR_EQUAL_TO -> first >= second;
         };
     }
 
     public boolean compare(short first, short second) {
         return switch (this) {
             case LESS_THAN -> first < second;
+            case LESS_THAN_OR_EQUAL_TO -> first <= second;
             case EQUAL_TO -> first == second;
             case GREATER_THAN -> first > second;
+            case GREATER_THAN_OR_EQUAL_TO -> first >= second;
         };
     }
 
     public boolean compare(byte first, byte second) {
         return switch (this) {
             case LESS_THAN -> first < second;
+            case LESS_THAN_OR_EQUAL_TO -> first <= second;
             case EQUAL_TO -> first == second;
             case GREATER_THAN -> first > second;
+            case GREATER_THAN_OR_EQUAL_TO -> first >= second;
         };
     }
 
@@ -65,8 +80,10 @@ public enum CompareCondition {
 
         return switch (this) {
             case LESS_THAN -> result < 0;
+            case LESS_THAN_OR_EQUAL_TO -> result <= 0;
             case EQUAL_TO -> result == 0;
             case GREATER_THAN -> result > 0;
+            case GREATER_THAN_OR_EQUAL_TO -> result >= 0;
         };
     }
 }
