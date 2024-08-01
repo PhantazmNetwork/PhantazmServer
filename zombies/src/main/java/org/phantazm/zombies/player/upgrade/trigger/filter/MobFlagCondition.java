5e5b7397de3ac54691356bd8d1d96f020ecc75fb
steank <dnd.initiative.tracker@gmail.com>
2024-08-01 10:52:27 -0700

    feat: MobFlagCondition

---
+++ zombies/src/main/java/org/phantazm/zombies/player/upgrade/trigger/filter/MobFlagCondition.java
@@ -0,0 +1,67 @@
+package org.phantazm.zombies.player.upgrade.trigger.filter;
+
+import com.github.steanky.element.core.annotation.Cache;
+import com.github.steanky.element.core.annotation.DataObject;
+import com.github.steanky.element.core.annotation.FactoryMethod;
+import com.github.steanky.element.core.annotation.Model;
+import com.github.steanky.ethylene.mapper.annotation.Default;
+import net.kyori.adventure.key.Key;
+import net.minestom.server.entity.Entity;
+import net.minestom.server.event.trait.EntityEvent;
+import org.jetbrains.annotations.NotNull;
+import org.phantazm.commons.InjectionStore;
+import org.phantazm.mob2.Mob;
+import org.phantazm.zombies.event.trait.EntityTargetEvent;
+import org.phantazm.zombies.player.ZombiesPlayer;
+
+@Model("zombies.upgrade.filter.condition.mob_flag")
+@Cache
+public class MobFlagCondition implements EventConditionComponent {
+    private final Data data;
+
+    @FactoryMethod
+    public MobFlagCondition(@NotNull Data data) {
+        this.data = data;
+    }
+
+    @Override
+    public @NotNull EventCondition<?> apply(@NotNull InjectionStore injectionStore, @NotNull ZombiesPlayer zombiesPlayer) {
+        return new Internal(data);
+    }
+
+    private record Internal(Data data) implements EventCondition<EntityEvent> {
+        @Override
+        public @NotNull Class<EntityEvent> eventType() {
+            return EntityEvent.class;
+        }
+
+        @Override
+        public boolean filter(@NotNull EntityEvent event) {
+            Entity entity;
+            if (data.useTarget && event instanceof EntityTargetEvent targetEvent) {
+                entity = targetEvent.target();
+            } else {
+                entity = event.getEntity();
+            }
+
+            if (!(entity instanceof Mob mob)) {
+                return false;
+            }
+
+            return mob.data().tags().contains(data.flag) != data.invert;
+        }
+    }
+
+    @Default("""
+        {
+          useTarget=false,
+          invert=false
+        }
+        """)
+    @DataObject
+    public record Data(@NotNull Key flag,
+        boolean useTarget,
+        boolean invert) {
+
+    }
+}
