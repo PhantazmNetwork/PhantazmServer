daa55a91b21651c4c4108c93e533b7a449d1c76b
steank <dnd.initiative.tracker@gmail.com>
2024-08-04 13:56:51 -0700

    fix: rooms can't be opened unless they have at least 1 adjacent room opened

--- zombies/src/main/java/org/phantazm/zombies/map/handler/BasicDoorHandler.java
+++ zombies/src/main/java/org/phantazm/zombies/map/handler/BasicDoorHandler.java
@@ -45,60 +45,77 @@ public class BasicDoorHandler implements DoorHandler {
 
         Wrapper<Boolean> handled = Wrapper.of(false);
         doorTracker.atPoint(clicked).ifPresent(door -> {
+            if (door.isOpen() || !player.canOpenDoor(door)) {
+                return;
+            }
+
             Optional<Player> playerOptional = player.getPlayer();
             if (playerOptional.isEmpty()) {
                 return;
             }
 
             Player actualPlayer = playerOptional.get();
-            if (!door.isOpen() && player.canOpenDoor(door)) {
-                Optional<Room> standingInRoomOptional = roomTracker.atPoint(actualPlayer.getPosition());
-                if (standingInRoomOptional.isEmpty() || !standingInRoomOptional.get().isOpen()) {
-                    return;
-                }
-
-                Room standingInRoom = standingInRoomOptional.get();
-                if (!standingInRoom.isOpen()) {
-                    return;
-                }
-
-                ZombiesPlayerOpenDoorEvent event = new ZombiesPlayerOpenDoorEvent(actualPlayer, player, door);
-                zombiesScene.get().broadcastEvent(event);
-                if (event.isCancelled()) {
-                    return;
-                }
-
-                Key standingIn = standingInRoom.getRoomInfo().id();
-
-                DoorInfo info = door.doorInfo();
-                List<Key> opensTo = info.opensTo();
-                List<Integer> costs = info.costs();
-
-                int sumCost = 0;
-                int minSize = Math.min(opensTo.size(), costs.size());
-                for (int i = 0; i < minSize; i++) {
-                    Key target = opensTo.get(i);
-                    if (target.equals(standingIn)) {
-                        continue;
-                    }
-
-                    sumCost += costs.get(i);
-                }
-
-                PlayerCoins coins = player.module().getCoins();
-                TransactionModifierSource modifiers = player.module().compositeTransactionModifiers();
-                TransactionResult result = coins.runTransaction(
-                    new Transaction(modifiers.modifiers(ModifierSourceGroups.DOOR_COIN_SPEND), -sumCost));
-
-                if (result.isAffordable(coins)) {
-                    coins.applyTransaction(result);
-                    door.open(player);
-                } else {
-                    door.failOpen(player);
-                }
-
-                handled.set(true);
+            Optional<Room> standingInRoomOptional = roomTracker.atPoint(actualPlayer.getPosition());
+            if (standingInRoomOptional.isEmpty() || !standingInRoomOptional.get().isOpen()) {
+                return;
             }
+
+            Room standingInRoom = standingInRoomOptional.get();
+            if (!standingInRoom.isOpen()) {
+                return;
+            }
+
+            ZombiesScene zombiesScene = this.zombiesScene.get();
+            boolean anyOpen = false;
+            for (Key otherRoom : door.doorInfo().opensTo()) {
+                Room room = zombiesScene.map().objects().roomMap().get(otherRoom);
+                if (room != null && room.isOpen()) {
+                    anyOpen = true;
+                    break;
+                }
+            }
+
+            if (!anyOpen) {
+                // can't open rooms that don't have any adjacent rooms open
+                return;
+            }
+
+            ZombiesPlayerOpenDoorEvent event = new ZombiesPlayerOpenDoorEvent(actualPlayer, player, door);
+            zombiesScene.broadcastEvent(event);
+            if (event.isCancelled()) {
+                return;
+            }
+
+            Key standingIn = standingInRoom.getRoomInfo().id();
+
+            DoorInfo info = door.doorInfo();
+            List<Key> opensTo = info.opensTo();
+            List<Integer> costs = info.costs();
+
+            int sumCost = 0;
+            int minSize = Math.min(opensTo.size(), costs.size());
+            for (int i = 0; i < minSize; i++) {
+                Key target = opensTo.get(i);
+                if (target.equals(standingIn)) {
+                    continue;
+                }
+
+                sumCost += costs.get(i);
+            }
+
+            PlayerCoins coins = player.module().getCoins();
+            TransactionModifierSource modifiers = player.module().compositeTransactionModifiers();
+            TransactionResult result = coins.runTransaction(
+                new Transaction(modifiers.modifiers(ModifierSourceGroups.DOOR_COIN_SPEND), -sumCost));
+
+            if (result.isAffordable(coins)) {
+                coins.applyTransaction(result);
+                door.open(player);
+            } else {
+                door.failOpen(player);
+            }
+
+            handled.set(true);
         });
 
         return handled.get();
