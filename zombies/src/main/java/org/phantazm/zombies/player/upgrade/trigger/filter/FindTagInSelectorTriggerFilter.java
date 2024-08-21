665e055831696e8472474822afd15d3f9bb43e8b
MyLadyMalady <cobrinthine.amaranthine@gmail.com>
2024-08-21 20:41:57 +0100

    Add new filter type

---
+++ zombies/src/main/java/org/phantazm/zombies/player/upgrade/trigger/filter/FindTagInSelectorTriggerFilter.java
@@ -0,0 +1,77 @@
+package org.phantazm.zombies.player.upgrade.trigger.filter;
+
+import com.github.steanky.element.core.annotation.Cache;
+import com.github.steanky.element.core.annotation.Child;
+import com.github.steanky.element.core.annotation.FactoryMethod;
+import com.github.steanky.element.core.annotation.Model;
+import com.github.steanky.ethylene.mapper.annotation.Default;
+import net.minestom.server.entity.Entity;
+import net.minestom.server.tag.Tag;
+import org.jetbrains.annotations.NotNull;
+import org.phantazm.commons.InjectionStore;
+import org.phantazm.core.CompareCondition;
+import org.phantazm.zombies.player.ZombiesPlayer;
+import org.phantazm.zombies.player.upgrade.PlayerUpgrade;
+import org.phantazm.zombies.player.upgrade.selector.Selector;
+import org.phantazm.zombies.player.upgrade.selector.SelectorComponent;
+import org.phantazm.zombies.player.upgrade.trigger.TriggerData;
+
+import java.util.Collection;
+
+@Model("zombies.upgrade.filter.find_tag_in_selector")
+@Cache
+public class FindTagInSelectorTriggerFilter implements TriggerFilterComponent {
+    private final Data data;
+    private final SelectorComponent selectorComponent;
+
+    @FactoryMethod
+    public FindTagInSelectorTriggerFilter(@NotNull Data data, @NotNull @Child("selector") SelectorComponent selectorComponent) {
+        this.data = data;
+        this.selectorComponent = selectorComponent;
+    }
+
+    @Override
+    public @NotNull TriggerFilter apply(@NotNull InjectionStore injectionStore, @NotNull ZombiesPlayer zombiesPlayer) {
+        return new Internal(data, selectorComponent.apply(injectionStore, zombiesPlayer), Tag.Integer(data.tag).defaultValue(data.defaultIfTagNotPresent));
+    }
+
+    private record Internal(Data data, Selector selector, Tag<Integer> tag) implements TriggerFilter {
+        public boolean test(@NotNull PlayerUpgrade upgrade, @NotNull ZombiesPlayer zombiesPlayer, @NotNull TriggerData triggerData) {
+            Collection<? extends Entity> targets = selector.select(upgrade, zombiesPlayer, triggerData).targets();
+
+            if(targets.isEmpty()) {
+                return false;
+            }
+
+            if(data.allTargetsMustMatch) {
+                for(Entity target : targets) {
+                    if(!data.condition.compare(data.value, target.getTag(tag))) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            for(Entity target : targets) {
+                if(data.condition.compare(data.value, target.getTag(tag))) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    @Default("""
+        {
+          allTargetsMustMatch=true,
+          condition=EQUAL_TO,
+          defaultIfTagNotPresent=0
+        }
+        """)
+    public record Data(@NotNull String tag,
+        boolean allTargetsMustMatch,
+        @NotNull CompareCondition condition,
+        int value,
+        int defaultIfTagNotPresent) {}
+}
