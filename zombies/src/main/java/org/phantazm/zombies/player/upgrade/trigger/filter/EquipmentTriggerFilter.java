967a27724a989ba1c89fad759960c72ca58f9181
steank <dnd.initiative.tracker@gmail.com>
2025-03-19 12:00:17 -0700

    add trigger filter for equipment key

---
+++ zombies/src/main/java/org/phantazm/zombies/player/upgrade/trigger/filter/EquipmentTriggerFilter.java
@@ -0,0 +1,43 @@
+package org.phantazm.zombies.player.upgrade.trigger.filter;
+
+import com.github.steanky.element.core.annotation.*;
+import org.jetbrains.annotations.NotNull;
+import org.phantazm.commons.InjectionStore;
+import org.phantazm.zombies.event.trait.EquipmentEvent;
+import org.phantazm.zombies.player.ZombiesPlayer;
+import org.phantazm.zombies.player.upgrade.PlayerUpgrade;
+import org.phantazm.zombies.player.upgrade.selector.Selector;
+import org.phantazm.zombies.player.upgrade.selector.SelectorComponent;
+import org.phantazm.zombies.player.upgrade.trigger.TriggerData;
+
+@Model("zombies.upgrade.filter.equipment")
+@Cache
+public class EquipmentTriggerFilter implements TriggerFilterComponent {
+    private final Data data;
+
+    @FactoryMethod
+    public EquipmentTriggerFilter(@NotNull Data data) {
+        this.data = data;
+    }
+
+    @Override
+    public @NotNull TriggerFilter apply(@NotNull InjectionStore injectionStore, @NotNull ZombiesPlayer zombiesPlayer) {
+        return new Internal(data);
+    }
+
+    private record Internal(Data data) implements TriggerFilter {
+        @Override
+        public boolean test(@NotNull PlayerUpgrade upgrade, @NotNull ZombiesPlayer zombiesPlayer, @NotNull TriggerData triggerData) {
+            if (triggerData.raw() instanceof EquipmentEvent event) {
+                return event.equipment().key().asString().equals(data.equipment);
+            }
+
+            return false;
+        }
+    }
+
+    @DataObject
+    public record Data(String equipment) {
+
+    }
+}
