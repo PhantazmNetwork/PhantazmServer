66d7d67f4fe838c129ee7585721b0c7ac6ba7d59
steank <dnd.initiative.tracker@gmail.com>
2025-04-21 15:57:36 -0700

    add CooldownInteractor

---
+++ zombies/src/main/java/org/phantazm/zombies/map/shop/interactor/CooldownInteractor.java
@@ -0,0 +1,56 @@
+package org.phantazm.zombies.map.shop.interactor;
+
+import com.github.steanky.element.core.annotation.*;
+import org.jetbrains.annotations.NotNull;
+import org.phantazm.core.Cooldown;
+import org.phantazm.zombies.map.shop.PlayerInteraction;
+import org.phantazm.zombies.map.shop.Shop;
+
+import java.util.List;
+import java.util.Objects;
+
+@Model("zombies.map.shop.interactor.cooldown")
+@Cache(false)
+public class CooldownInteractor extends InteractorBase<CooldownInteractor.Data> {
+    private final List<ShopInteractor> successInteractors;
+    private final List<ShopInteractor> failureInteractors;
+    private final Cooldown cooldown;
+
+    @FactoryMethod
+    public CooldownInteractor(@NotNull Data data,
+        @Child("successInteractors") List<ShopInteractor> successInteractors,
+        @Child("failureInteractors") List<ShopInteractor> failureInteractors) {
+        super(data);
+        this.successInteractors = Objects.requireNonNull(successInteractors);
+        this.failureInteractors = Objects.requireNonNull(failureInteractors);
+        this.cooldown = Cooldown.cooldown(0);
+    }
+
+    @Override
+    public boolean handleInteraction(@NotNull PlayerInteraction interaction) {
+        if (data.cooldown < 0 || cooldown.takeCooldown(data.cooldown))
+            ShopInteractor.handle(successInteractors, interaction);
+        else ShopInteractor.handle(failureInteractors, interaction);
+
+        return true;
+    }
+
+    @Override
+    public void tick(long time) {
+        ShopInteractor.tick(successInteractors, time);
+        ShopInteractor.tick(failureInteractors, time);
+
+        cooldown.step();
+    }
+
+    @Override
+    public void initialize(@NotNull Shop shop) {
+        ShopInteractor.initialize(successInteractors, shop);
+        ShopInteractor.initialize(failureInteractors, shop);
+    }
+
+    @DataObject
+    public record Data(int cooldown) {
+
+    }
+}
