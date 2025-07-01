dc275a98694b73a973e2417a430cb7704158dda2
steank <dnd.initiative.tracker@gmail.com>
2025-07-01 02:40:16 -0700

    add mob.validator.not

---
+++ mob/src/main/java/org/phantazm/mob2/validator/NotValidator.java
@@ -0,0 +1,46 @@
+package org.phantazm.mob2.validator;
+
+import com.github.steanky.element.core.annotation.Cache;
+import com.github.steanky.element.core.annotation.Child;
+import com.github.steanky.element.core.annotation.FactoryMethod;
+import com.github.steanky.element.core.annotation.Model;
+import net.minestom.server.entity.Entity;
+import org.jetbrains.annotations.NotNull;
+import org.phantazm.mob2.Mob;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Model("mob.validator.not")
+@Cache
+public class NotValidator implements ValidatorComponent {
+    private final List<ValidatorComponent> validators;
+
+    @FactoryMethod
+    public NotValidator(@NotNull @Child("validators") List<ValidatorComponent> validators) {
+        this.validators = validators;
+    }
+
+    @Override
+    public @NotNull Validator get() {
+        List<Validator> validators = new ArrayList<>(this.validators.size());
+        for (ValidatorComponent component : this.validators) {
+            validators.add(component.get());
+        }
+
+        return new Internal(validators);
+    }
+
+    private record Internal(List<Validator> validators) implements Validator {
+        @Override
+        public boolean valid(@NotNull Mob mob, @NotNull Entity entity) {
+            for (Validator validator : validators) {
+                if (validator.valid(mob, entity)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
