b6dddcdf61c38dbf4f633cc46be2f7520f372823
steank <dnd.initiative.tracker@gmail.com>
2025-07-01 03:56:20 -0700

    why is cob so picky about the exact naming of this config field smh

--- zombies/src/main/java/org/phantazm/zombies/equipment/gun/target/tester/PhantazmMobTargetTester.java
+++ zombies/src/main/java/org/phantazm/zombies/equipment/gun/target/tester/PhantazmMobTargetTester.java
@@ -5,6 +5,7 @@ import com.github.steanky.element.core.annotation.FactoryMethod;
 import com.github.steanky.element.core.annotation.Model;
 import net.minestom.server.entity.Entity;
 import org.jetbrains.annotations.NotNull;
+import org.phantazm.mob2.Mob;
 import org.phantazm.zombies.equipment.gun.Gun;
 import org.phantazm.zombies.event.equipment.GunTargetSelectEvent;
 import org.phantazm.zombies.map.objects.MapObjects;
@@ -31,7 +32,13 @@ public class PhantazmMobTargetTester implements TargetTester {
             .map(round -> {
                 GunTargetSelectEvent selectEvent = new GunTargetSelectEvent(target, gun);
                 zombiesScene.broadcastEvent(selectEvent);
-                return selectEvent.isForceSelected() || round.hasMob(target.getUuid());
+
+                boolean isForceSelected = selectEvent.isForceSelected();
+
+                if (target instanceof Mob mob && mob.data().extra().getBooleanOrDefault("force_deselect", false))
+                    return isForceSelected;
+
+                return isForceSelected || round.hasMob(target.getUuid());
             }).orElse(false);
     }
 }
